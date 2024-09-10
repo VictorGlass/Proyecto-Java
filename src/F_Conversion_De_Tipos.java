b4c6a14ab99bedb5b6acbfb96023077c1122f7d5
VictorGlass <enginemaster551@gmail.com>
2024-09-10 15:44:48 -0300

    Agregando el dia 6

---
+++ src/F_Conversion_De_Tipos.java
@@ -0,0 +1,38 @@
+// Dia 6 - Conversion de tipos.
+
+/*
+ * 1. Escribe un programa que realice estas tareas.
+ * 
+ * * Convertir una cadena en un numero entero.
+ * * Convertir una cadena en un float.
+ * * Convertir un numero entero en una cadena mediante el metodo valueOf().
+ * * Convertir un numero entero en una cadena mediante el metodo toString().
+ * 
+ */
+
+//SOLUCION:
+
+public class F_Conversion_De_Tipos {
+    public static void main(String[] args) {
+        
+
+        //Convertir una cadena en un numero entero.
+        String cadenaEntero = "123";
+        int numeroEntero = Integer.parseInt(cadenaEntero);
+        System.out.println("Cadena convertida en entero: " + numeroEntero);
+
+        //Convertir una cadena en un float.
+        String cadenaFloat = "123,45";
+        float numeroFloat = Float.parseFloat(cadenaFloat);
+        System.out.println("Cadena convertida a float: " + numeroFloat);
+
+        //Convertir un numero entero en una cadena mediante el metodo valueOf().
+        int numero = 456;
+        String cadenaDesdeValueOf = String.valueOf(numero);
+        System.out.println("Numero entero convertifo a cadena usando valueOf: " + cadenaDesdeValueOf);
+
+        //Convertir un numero entero en una cadena usando toString().
+        String cadenaDesdeToString = Integer.toString(numero);
+        System.out.println("Entero convertido en cadena usando toString(): " + cadenaDesdeToString);
+    }
+}
