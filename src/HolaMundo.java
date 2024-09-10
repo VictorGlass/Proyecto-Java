aaa31504b00b5b071b8b48235093239b01a844ff
VictorGlass <enginemaster551@gmail.com>
2024-09-10 15:21:33 -0300

    Creando el Proyecto

---
+++ src/HolaMundo.java
@@ -0,0 +1,51 @@
+// Dia 1 - Hola Mundo.
+
+/*
+ * 1.Escribe un programa que imprima "¡Hola Mundo!" en consola.
+ * 2. Agregue comentarios de una sola linea y de varias lineas a su codigo.
+ */
+
+
+
+//SOLUCION:
+
+//1.
+
+public class HolaMundo {
+    public static void main(String[] args) throws Exception {
+
+        String texto = "¡Hola Mundo!";
+
+        System.out.println(texto);
+    }
+}
+
+
+//2.
+
+ //Esto es un comentario de una sola linea.
+
+ /*
+  * Esto es un comentario
+  de varias
+  lineas
+  en VSCode usando Java.
+  */
+
+  //Datos Java:
+
+  /**
+   * 1. Aprende la diferencia entre Java y Python.
+   * R: Java es compilado, tiene una sintaxis mas detallada, es estaticamente tipado,
+   * se utiliza en aplicaciones empresariales, android y escritorio.
+   *    Python es interpretado, sintaxis mas sencilla, dinamicamente tipado,
+   * se utiliza en desarrollo web, ciencia de datos, analisis de datos y automatizacion.
+   * 
+   * 2. ¿Que es un compilador Java y como funciona?.
+   * R: Es una herramiento que convierte el codigo fuente de Java(archivo.java) en
+   * bytecode, un formato intermedio que la Java Vitrual Machine(JVM) puede ejectutar.
+   * 
+   * 3. Diferencia entre compilador e interprete.
+   * R: Compilador: Traduce el codigo todo a la vez, creando un archivo ejecutable. ejemplo C, Java.
+   * Interprete: Traduce y ejecuta el codigo linea por linea, sin crear un archivo intermedio,. ejemplo Python, JavaScript.
+   */
